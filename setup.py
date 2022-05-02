7f3cda2cdacc7632d1714498637e2dadf949d39c
Vu Nguyen <ntienvu@gmail.com>
2022-05-02 13:58:49 +0930

    fix setup file

--- setup.py
+++ setup.py
@@ -3,17 +3,19 @@ from setuptools import setup, find_packages
 setup(
     name='mini_bo',
     version='1',
-    packages = ["bayes_opt",
-    "mini_bo.functions",
-    "mini_bo.gp",
-	"mini_bo.utility",
-	"mini_bo.visualization"],
+    packages=find_packages(),
+    #packages = ["mini_bo",
+    #"mini_bo.gp",
+    #"mini_bo.functions",
+	#"mini_bo.utility",
+	#"mini_bo.visualization"],
     include_package_data = True,
     description='Mini Bayesian Optimization',
     install_requires=[
         "numpy >= 1.9.0",
         "scipy >= 0.14.0",
         "scikit-learn >= 0.16.1",
-        "tabulate">=0.8.7",
+        "tabulate>=0.8.7",
+        "matplotlib>=3.1.0"
     ],
 )
